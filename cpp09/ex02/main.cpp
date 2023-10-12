224844a8ffa664ef019854af57eef2f000aa70c7
Tsvetoslav Vasilev <tsvetivasilev@gmail.com>
2023-10-12 09:43:52 +0200

    doesnt work with odd numbers

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -9,21 +9,15 @@ int	main(int argc, char **argv)
 	}
 
 	PmergeMe	p;
-
-	p.insert(10);
-	p.insert(122);
-	p.insert(1552);
-	p.insert(15);
-	p.insert(50);
-	p.insert(1);
-	p.insert(13);
-	p.insert(77);
-	p.insert(0);
-	p.insert(88);
+	for (size_t i = 1; i < argc; i++)
+	{
+		p.insert(std::atoi(argv[i]));
+	}
+	
+	std::cout << "Before:" << std::endl;
 	p.print();
-	std::cout << "After:" << std::endl;
 	p.sort();
-	std::cout << "After sorting:" << std::endl;
+	std::cout << "After:" << std::endl;
 	p.print();
 	return (0);
 }
