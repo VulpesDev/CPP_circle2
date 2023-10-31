785390d0493bcff7773696102d92e13541838833
Tsvetoslav Vasilev <tsvetivasilev@gmail.com>
2023-10-31 12:13:27 +0100

    fixed the print error

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -41,7 +41,7 @@ int	main(int argc, char **argv)
 	std::cout << "After:" << std::endl;
 	p.print();
 	std::cout << "Time to process a range of " << p.size() << " elements with std::vector : "  << std::fixed << static_cast<double>(end_time_vec - start_time_vec)/CLOCKS_PER_SEC << "s" << std::endl;
-	std::cout << "Time to process a range of " << p.size() << " elements with std::vector : "  << std::fixed << static_cast<double>(end_time_deque - start_time_deque)/CLOCKS_PER_SEC << "s" << std::endl;
+	std::cout << "Time to process a range of " << p.size() << " elements with std::deque : "  << std::fixed << static_cast<double>(end_time_deque - start_time_deque)/CLOCKS_PER_SEC << "s" << std::endl;
 
 	return (0);
 }
